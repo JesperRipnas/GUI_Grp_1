4f03c85d591cefe93350c8d824248e9d13a766ca
Oscar Carlsson <Oscar.carlsson@LIVE.COM>
2021-10-20 16:57:46 +0200

    broken af

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -25,11 +26,6 @@ namespace GUI
             InitializeComponent();
         }
 
-        private void Exit_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-
-        }
-
         private void Menu_MouseDown(object sender, MouseButtonEventArgs e)
         {
             try
@@ -40,6 +36,52 @@ namespace GUI
             {
 
             }
+        }
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        Storyboard storyboard;
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button btn = (Button)sender;
+            Color background = ((SolidColorBrush)btn.BorderBrush).Color;
+            storyboard = new Storyboard();
+            storyboard.Children.Add(SetAnimButton(background, btn.Name));
+            storyboard.Children.Add(SetAnimCirkle(background));
+            storyboard.Begin(this);
+        }
+
+        private void Button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Button btn = (Button)sender;
+            storyboard = new Storyboard();
+            storyboard.Children.Add(SetAnimButton(Color.FromRgb(113, 110, 110), btn.Name));
+            storyboard.Children.Add(SetAnimCirkle(Color.FromArgb(150, 67, 67, 67)));
+            storyboard.Begin(this);
+        }
+
+        public ColorAnimation SetAnimButton(Color Color, string objName)
+        {
+            ColorAnimation anim = new ColorAnimation();
+            anim.Duration = new Duration(TimeSpan.FromSeconds(0.2));
+            anim.To = Color;
+            Storyboard.SetTargetName(anim, objName);
+            Storyboard.SetTargetProperty(anim, new PropertyPath("(Button.Background).(SolidColorBrush.Color)"));
+            return anim;
+        }
+
+        public ColorAnimation SetAnimCirkle(Color Color)
+        {
+            ColorAnimation anim = new ColorAnimation();
+            anim.Duration = new Duration(TimeSpan.FromSeconds(0.2));
+            anim.To = Color;
+            Storyboard.SetTargetName(anim, "ColorCirkle");
+            Storyboard.SetTargetProperty(anim, new PropertyPath(GradientStop.ColorProperty));
+            return anim;
         }
     }
 }
